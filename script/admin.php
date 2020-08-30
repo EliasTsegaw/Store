9f5ef06a6599207f51e02531fc635c0a36dee7c3
EliasTsegaw <tsegawelias@gmail.com>
2020-08-30 17:39:47 -0400

    Working on admin page

---
+++ script/admin.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Admin</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://kit.fontawesome.com/6f27ed0381.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="">
+</head>
+
+<body>
+    <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+    <div class="fixed-header">
+        <b> Adama Science and Technology University </b>
+        <img class="fit-picture" src="../images/astu.jpg" alt="University Logo">
+        <b>ECE Store</b>
+        <div class="wrapper">
+            <input type="text" class="input" placeholder="What are you looking for?">
+            <div class="searchbtn"><i class="fas fa-search"></i></div>
+        </div>
+        <div class="container">
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Products</a>
+                <a href="#">Services</a>
+                <a href="#">Contact Us</a>
+            </nav>
+        </div>
+    </div>
+
+    <div class="mobile_nav">
+        <div class="nav_bar">
+            <i class="material-icons">menu</i>
+        </div>
+        <a href="#"><i class="fas fa-newspaper"></i></a>
+        <a href="#"><i class="fas fa-envelope"></i></A>
+        <a href="#"><i class="fas fa-database"></i></a>
+        <a href="#"><span><b><i class="material-icons">backup</i></a>
+        <a href="#"><i class="fas fa-chart-line"></i></a>
+        <a href="#"><i class="fas fa-calendar-alt"></i></a>
+    </div>
+
+    <div class="sidebar">
+        <a href="#"><i class="fas fa-newspaper"><span>Manage news</span></i></a>
+        <a href="#"><i class="fas fa-envelope"><span>Manage Email</span></i></A>
+        <a href="#"><i class="fas fa-database"><span>Manage Database</span></i></a>
+        <a href="#"><span><b><i class="material-icons">backup</i> System Backup</b></span></a>
+        <a href="#"><i class="fas fa-chart-line"><span>System Report</span></i></a>
+        <a href="#"><i class="fas fa-calendar-alt"><span>Calendar</span></i></a>
+    </div>
+
+    <div class="fixed-footer"></div>
+    <div class="container">
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Products</a>
+            <a href="#">Services</a>
+            <a href="#">Contact Us</a>
+        </nav>
+    </div>
+    </div>
+    <script src="" async defer></script>
+</body>
+
+</html>
